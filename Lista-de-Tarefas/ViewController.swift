a6c7219dfe42e50e1e9c5c26b4350bb89b2d8905
Felipe Weber <weberecomp@gmail.com>
2019-11-28 09:58:41 -0300

    Initial Commit

---
+++ Lista-de-Tarefas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lista-de-Tarefas
+//
+//  Created by Felipe Weber on 28/11/19.
+//  Copyright © 2019 Felipe Weber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
